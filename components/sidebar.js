041d43eeefa8763377bee633e36b40e7e2e3d2bc
yeyouchuan <yeyouchuan@onmail.com>
2023-03-25 00:11:22 +0000

    加载网站

--- components/sidebar.js
+++ components/sidebar.js
@@ -3,7 +3,7 @@ import {useState} from "react";
 export default function Sidebar() {
     return (
         <>
-            <div className="z-100 h-screen sticky top-0 overflow-auto bg-white pt-6 pb-10 md:flex flex-col flex-none text-sm w-full max-w-[220px] px-1">
+            <div className="z-100 h-screen sticky top-0 overflow-auto bg-white pt-6 pb-10 flex-col flex-none text-sm w-full max-w-[220px] px-1">
                 <a href="/">赴山茶市</a>
                 <a>最新简讯</a>
             </div>
