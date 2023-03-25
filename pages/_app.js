0b4ab8bacf2d11f27d72e7b3db9d42a0302eb595
yeyouchuan <yeyouchuan@onmail.com>
2023-03-25 01:14:29 +0000

    加载网站

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import '@/styles/globals.css'
-import MainLayout from "layouts/main";
 
 export default function App({ Component, pageProps }) {
   return (
-      <MainLayout>
-          <Component {...pageProps} />
-      </MainLayout>
+      <Component {...pageProps} />
   )
 
 }
