fb85956e784ced4112ca40389cdedf1af2f2f2ec
yeyouchuan <yeyouchuan@onmail.com>
2023-03-25 01:05:58 +0000

    加载网站

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-          <div></div>
+          <div className="">测试</div>
       </main>
     </>
   )
